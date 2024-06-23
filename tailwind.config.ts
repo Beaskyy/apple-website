0721b69b6e8e226191246b8f56545ef33deeb16d
Beaskyy <afeez.akintobi@yahoo.com>
2024-06-23 12:42:09 +0100

    setup for the webpage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
+      },
+    },
   },
   plugins: [],
 };
