c0913dac3a63d6c2e7656879ff4993d584f2f554
Beaskyy <afeez.akintobi@yahoo.com>
2024-06-28 22:00:29 +0100

    animation

--- components/ModelView.tsx
+++ components/ModelView.tsx
@@ -9,13 +9,14 @@ import Iphone from "./IPhone";
 import { Suspense } from "react";
 
 import * as THREE from "three";
+import Loader from "./Loader";
 
 interface ModelViewProps {
   index: number;
   groupRef: any;
   gsapType: string;
   controlRef: any;
-  setRotation: any;
+  setRotationState: any;
   item: {
     title: string;
     color: string[];
@@ -28,7 +29,7 @@ const ModelView = ({
   groupRef,
   gsapType,
   controlRef,
-  setRotation,
+  setRotationState,
   item,
   size,
 }: ModelViewProps) => {
@@ -36,10 +37,10 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full ${index === 2 && "right-[-100%]"}`}
+      className={`w-full h-full absolute ${index === 2 && "right-[-100%]"}`}
     >
       {/* Ambient Light */}
-      <ambientLight intensity={0.3} />
+      <ambientLight intensity={0. 3} />
 
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
@@ -52,7 +53,7 @@ const ModelView = ({
         enablePan={false}
         rotateSpeed={0.4}
         target={new THREE.Vector3(0, 0, 0)}
-        onEnd={() => setRotation(controlRef.current.getAzimuthalAngle())}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
       />
 
       <group
@@ -62,12 +63,14 @@ const ModelView = ({
       >
         <Suspense
           fallback={
-            <Html>
-              <div>Loading</div>
-            </Html>
+            <Loader />
           }
         >
-          <Iphone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} item={item} size={size} />
+          <Iphone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
         </Suspense>
       </group>
     </View>
